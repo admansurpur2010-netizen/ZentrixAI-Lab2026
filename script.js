464678184169a87c26c9a3825815b02e2e33934c
admansurpur2010-netizen <admansurpur2010@gmail.com>
2026-03-16 07:56:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,65 @@
 // SIDEBAR TOGGLE
+
 const menuToggle = document.getElementById("menuToggle");
 const sidebar = document.getElementById("sidebar");
 
-if(menuToggle){
 menuToggle.addEventListener("click", () => {
-sidebar.classList.toggle("showSidebar");
-});
+
+if(sidebar.classList.contains("hideSidebar")){
+sidebar.classList.remove("hideSidebar");
+sidebar.classList.add("showSidebar");
+}else{
+sidebar.classList.add("hideSidebar");
 }
+
+});
 
 
 // THREE DOT MENU
+
 const threeDotBtn = document.getElementById("threeDotBtn");
 const threeDotMenu = document.getElementById("threeDotMenu");
 
-if(threeDotBtn){
 threeDotBtn.addEventListener("click", () => {
 threeDotMenu.classList.toggle("showMenu");
 });
-}
 
 
-// PLUS TOOLS MENU
+// PLUS MENU
+
 const plusBtn = document.getElementById("plusBtn");
 const plusMenu = document.getElementById("plusMenu");
 
-if(plusBtn){
 plusBtn.addEventListener("click", () => {
 plusMenu.classList.toggle("showTools");
 });
-}
 
 
-// CLICK OUTSIDE CLOSE MENUS
+// CLOSE MENUS OUTSIDE CLICK
+
 document.addEventListener("click",(e)=>{
 
-if(threeDotBtn && !threeDotBtn.contains(e.target) && !threeDotMenu.contains(e.target)){
+if(!threeDotBtn.contains(e.target) && !threeDotMenu.contains(e.target)){
 threeDotMenu.classList.remove("showMenu");
 }
 
-if(plusBtn && !plusBtn.contains(e.target) && !plusMenu.contains(e.target)){
+if(!plusBtn.contains(e.target) && !plusMenu.contains(e.target)){
 plusMenu.classList.remove("showTools");
 }
 
 });
+
+
+// THEME TOGGLE
+
+function toggleTheme(){
+
+if(document.body.classList.contains("dark-theme")){
+document.body.classList.remove("dark-theme");
+document.body.classList.add("light-theme");
+}else{
+document.body.classList.remove("light-theme");
+document.body.classList.add("dark-theme");
+}
+
+  }
