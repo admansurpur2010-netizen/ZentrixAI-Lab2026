ce8eece7e5aab46428d6dc4ae7dbbe935ad35bb7
Aditya <admansurpur2010@gmail.com>
2026-03-17 00:16:21 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,21 +1,20 @@
 export default async function handler(req, res) {
 
-const HF_TOKEN = process.env.HF_TOKEN;console.log("HF TOKEN:", HF_TOKEN);
+const HF_TOKEN = process.env.HF_TOKEN;
 
-if(req.method !== "POST"){
-return res.status(405).json({error:"Method not allowed"});
+if (req.method !== "POST") {
+return res.status(405).json({ error: "Method not allowed" });
 }
 
-try{
+try {
 
 const response = await fetch(
-"https://router.huggingface.co/hf-inference/models/google/flan-t5-small",
+"https://api-inference.huggingface.co/models/google/flan-t5-small",
 {
-method:"POST",
-headers:{
-"Authorization": `Bearer ${HF_TOKEN}`,
-"Content-Type":"application/json",
-"Accept": "application/json"
+method: "POST",
+headers: {
+Authorization: `Bearer ${HF_TOKEN}`,
+"Content-Type": "application/json"
 },
 body: JSON.stringify({
 inputs: req.body.userText
@@ -27,8 +26,8 @@ const data = await response.json();
 
 res.status(200).json(data);
 
-}catch(err){
-res.status(500).json({error:"AI request failed"});
+} catch (err) {
+res.status(500).json({ error: "AI request failed" });
 }
 
 }
